0e25ce029688bf023230019a4d17ee0ec07741b6
Amr Aboelela <amraboelela@gmail.com>
2022-09-27 11:05:59 -0700

    Update Message.swift

--- Sources/SwiftForum/Models/Message.swift
+++ Sources/SwiftForum/Models/Message.swift
@@ -57,7 +57,9 @@ public struct Message: Codable, Equatable, Sendable {
 
     public static func messages(toUsername: String, fromUsername: String? = nil, nonReadOnly: Bool = false, count: Int = 200) async -> [Message] {
         var result = [Message]()
+        NSLog("messages toUsername: \(toUsername)")
         if let fromUsername = fromUsername {
+            NSLog("fromUsername: \(fromUsername)")
             await database.enumerateKeysAndValues(backward: true, andPrefix: prefix + toUsername + "-") { (key, message: Message, stop) in
                 if fromUsername == message.fromUsername &&
                     (!nonReadOnly || message.timeRead == nil) {
@@ -73,11 +75,13 @@ public struct Message: Codable, Equatable, Sendable {
             result = result.sorted { $0.timeSent < $1.timeSent }
         } else {
             await database.enumerateKeysAndValues(backward: true, andPrefix: prefix + toUsername + "-") { (key, message: Message, stop) in
+                NSLog("message: \(message)")
                 if !nonReadOnly || message.timeRead == nil {
                     result.append(message)
                 }
             }
         }
+        NSLog("result: \(result)")
         if result.count > count {
             result.removeFirst(result.count - count)
         }
